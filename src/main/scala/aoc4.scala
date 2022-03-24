330f489e0c267815df673c18bc39230df9fc7cd0
Cassandra <cassandrabunschoten@gmail.com>
2022-03-24 17:06:46 +0100

    start enum

--- src/main/scala/aoc4.scala
+++ src/main/scala/aoc4.scala
@@ -3,7 +3,7 @@ import scala.util.matching.Regex
 import scala.io.Source
 
 case class Passport(byr: Option[String], iyr: Option[String], eyr: Option[String], hgt: Option[String], 
-                   hcl: Option[String], ecl: Option[String], pid: Option[String], cid: Option[String]) {
+                   hcl: Option[String], ecl: Option[EyeColor], pid: Option[String], cid: Option[String]) {
   def check: Boolean =
    (byr, iyr, eyr, hgt, hcl, ecl, pid) match
      case (Some(_), Some(_), Some(_), Some(_), Some(_), Some(_), Some(_)) => true
@@ -14,19 +14,32 @@ case class Passport(byr: Option[String], iyr: Option[String], eyr: Option[String
     iyr.map(iyrValid).getOrElse(false) &
     eyr.map(eyrValid).getOrElse(false) &
     hgt.map(hgtValid).getOrElse(false) &
-    ecl.map(eclValid).getOrElse(false) 
+    ecl.exists(_ => true)
 }
 
+enum EyeColor(val clr: String):
+  case Amber extends EyeColor("amb")
+  case Blue  extends EyeColor("blu")
+  case Brown extends EyeColor("brn")
+  case Grey  extends EyeColor("gry")
+  case Green extends EyeColor("grn")
+  case Hazel extends EyeColor("hzl")
+  case Other extends EyeColor("oth")
+
+enum Birthyear(val year: Int):
+  case Min extends Birthyear(1929)
+  case Max extends Birthyear(2002)
+
 def byrValid(s: String): Boolean =
-  val year = s.substring(4).toInt 
-  year >= 1920 & year <= 2002
+  val year = s.substring(4).toInt
+  year >= 1929 & year <= 2002
 
 def iyrValid(s: String): Boolean =
-  val year = s.substring(4).toInt 
+  val year = s.substring(4).toInt
   year >= 2010 & year <= 2020
 
 def eyrValid(s: String): Boolean =
-  val year = s.substring(4).toInt 
+  val year = s.substring(4).toInt
   year >= 2020 & year <= 2030
 
 def hgtValid(s: String): Boolean =
@@ -35,8 +48,17 @@ def hgtValid(s: String): Boolean =
     case s if s.endsWith("in") => val hgt = s.substring(0, 2).toInt; hgt >= 59 & hgt <= 76
     case _ => false
 
-def eclValid(s: String): Boolean =
-  List("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(s.substring(4))
+def stringToEcl(s: String): Option[EyeColor] =
+  import EyeColor.*
+  s.substring(4) match
+    case Amber.clr  => Some(Amber)
+    case Blue.clr   => Some(Blue)
+    case Brown.clr  => Some(Brown)
+    case Grey.clr   => Some(Grey)
+    case Green.clr  => Some(Green)
+    case Hazel.clr  => Some(Hazel)
+    case Other.clr  => Some(Other)
+    case _          => None
 
 def stringToPassport(s: String): Passport =
    val byr = raw"byr:\d{4}".r.findFirstIn(s)
@@ -44,17 +66,19 @@ def stringToPassport(s: String): Passport =
    val eyr = raw"eyr:\d{4}".r.findFirstIn(s)
    val hgt = raw"hgt:(\d{3}cm|\d{2}in)".r.findFirstIn(s)
    val hcl = raw"hcl:#[0-9a-f]{6}".r.findFirstIn(s)
-   val ecl = raw"ecl:[a-z]{3}".r.findFirstIn(s)
+   val ecl = stringToEcl(raw"ecl:[a-z]{3}".r.findFirstIn(s).getOrElse("       "))
    val pid = "pid:[0-9]{9}(\\s|$)".r.findFirstIn(s)
    val cid = raw"cid:".r.findFirstIn(s)
    Passport(byr, iyr, eyr, hgt, hcl, ecl, pid, cid)
 
 object AOC42020 extends App:
-    println(Source
-    .fromFile("src/main/resources/input_aoc4.txt")
-    .mkString
-    .split("\n\n")
-    .map(_.map(_ match{ case '\n' => ' ' case x => x}))
-    .map(stringToPassport(_))
-    .filter(_.check)
-    .filter(_.valid).length)
+    // println(Source
+    // .fromFile("src/main/resources/input_aoc4.txt")
+    // .mkString
+    // .split("\n\n")
+    // .map(_.map(_ match{ case '\n' => ' ' case x => x}))
+    // .map(stringToPassport(_))
+    // .filter(_.check)
+    // .filter(_.valid).length)
+
+    println("EYES: " + stringToEcl("ecl:amb"))
