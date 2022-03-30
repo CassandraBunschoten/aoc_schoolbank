354affa901d6c2f84ac7cd718642aa76d493c2a4
Cassandra <cassandrabunschoten@gmail.com>
2022-03-30 21:46:04 +0200

    added day 1-3

---
+++ src/main/scala/aoc1.scala
@@ -0,0 +1,28 @@
+package aoc2020 
+
+import scala.io.Source
+
+object AOC12020 extends App:
+    val expenses = Source
+        .fromFile("fps/src/main/resources/input_aoc1.txt").getLines.map(_.toInt).toList
+
+    def comboTwo: (Int, Int, Int) = 
+      (for (
+        a <- expenses;
+        b <- expenses
+        if (a + b == 2020)
+    ) yield(a, b, a * b)).headOption.getOrElse(sys.error("Not found"))
+      
+
+    def comboThree: (Int, Int, Int, Int) =
+      (for (
+        a <- expenses;
+        b <- expenses;
+        c <- expenses
+        if (a + b + c == 2020)
+      ) yield(a, b, c, a * b * c)).headOption.getOrElse(sys.error("Not found"))
+
+    println(comboTwo)
+    println(comboThree)
+
+
---
+++ src/main/scala/aoc3.scala
@@ -0,0 +1,27 @@
+package aoc2020
+import scala.io.Source
+
+def move(l: List[(String, Int)], right: Int): Long = 
+  l.map(_ match 
+    case (string, int) => string.charAt((int * right) % string.length) == '#')
+    .count(_ == true)
+    .toLong
+
+object AOC32020 extends App:
+
+    val input = Source
+      .fromFile("fps/src/main/resources/input_aoc3.txt")
+      .getLines
+      .toList
+      .zipWithIndex
+
+      val move2 = 
+        input.
+          map(_ match
+            case (string, int) => if (int % 2 == 0) 
+              then string.charAt((int - (int /2)) % string.length) == '#' else false)
+              .count(_ == true)
+              .toLong
+
+    println(s"${move(input, 1)} * ${move(input, 3)} * ${move(input, 5)} * ${move(input, 7)} * $move2")
+    println(move(input, 1) * move(input, 3)* move(input, 5) * move(input, 7) * move2)
