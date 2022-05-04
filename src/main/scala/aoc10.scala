ec2c90f7ec8ab39b1882622865b6a55acaf58be8
Cassandra <cassandrabunschoten@gmail.com>
2022-05-04 14:09:04 +0200

    finished day 10

--- src/main/scala/aoc10.scala
+++ src/main/scala/aoc10.scala
@@ -1,19 +1,38 @@
 package aoc2020
 
 import scala.io.Source
+import scala.collection.immutable.*
 
 sealed trait Num
-
 case object One extends Num
 case object Two extends Num
 case object Three extends Num
 
-// def calculate(slide: List[Int]): Int =
-//   for(
-//     j <- slide.tail
-//     i  = slide.head
-//   )
+case class   IntNode(int: Int, branches: List[Int])
 
+sealed trait Tree
+case class   Node(int: Int, leaf: Long) extends Tree
+case object  Leaf extends Tree
+
+def makeNodes(lis: List[Int]): IntNode =
+  val i = lis.head
+  val b = lis.drop(1).filter(x => x == i + 1 || x ==  i + 2 || x == i + 3)
+  IntNode(i, b)
+
+def makeTree(lis: List[IntNode]): Long =
+  val seen = HashMap((lis.reverse.head.int, 1L))
+  val done = lis.reverse.drop(1).foldLeft(seen)((acc, elm) => 
+    val br = elm.branches.map(x => 
+      acc.get(x) match
+        case Some(x) => x
+        case None    => sys.error(s"Ik kan hem niet vinden ${x}"))
+    acc + ((elm.int, br.sum))
+  )
+  done.get(0) match
+    case Some(x) => x
+    case None    => sys.error("hoe heb je dit voor elkaar gekregen")
+
+  
 
 object AOC102020 extends App: 
   val input = Source
@@ -25,7 +44,6 @@ object AOC102020 extends App:
 
   val mid = 0 :: input ::: List(input.reverse.head + 3)
 
-
   val answer = mid.sliding(2)
     .map(x =>
       val t = x(0)
@@ -36,6 +54,10 @@ object AOC102020 extends App:
         case _ => throw(sys.error("help"))
     )
     .toList
+
     
-  println(mid)
-  println(answer.count(_ == One) *  (answer.count(_ == Three)))
+  val hackyMid = 0 :: input ::: List(input.reverse.head + 3, 0, 0)
+
+  val nodes = hackyMid.sliding(4).map(makeNodes(_)).toList
+  println(makeTree(nodes))
+  println(answer.count(_ == One) * (answer.count(_ == Three)))
