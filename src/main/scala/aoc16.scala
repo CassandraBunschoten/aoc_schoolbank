b502f6f0c1c84c106e3c7dccad01f03523961d31
Cassandra <cassandrabunschoten@gmail.com>
2022-08-03 17:27:18 +0200

    finished day 16, need to refactor later

--- src/main/scala/aoc16.scala
+++ src/main/scala/aoc16.scala
@@ -2,6 +2,10 @@ package aoc2020
 
 import scala.io.Source
 
+case class TicketRules(name: FieldName, firstRule: Range, secondRule: Range)
+
+type FieldName = String
+
 def toTicket(input: String): List[Ticket] =
   val tickets = input.split("\n").map(_.split(",").toList).toList
   val intTickets = tickets.map(x => x.map(_.toIntOption).flatten)
@@ -24,7 +28,6 @@ def toTicketRules(input: String): List[TicketRules] =
 
 case class Ticket(numbers: List[Int]):
   def doesNotComplyWithRule(rules: List[TicketRules]) =
-
     def boolComply(rules: List[TicketRules], numCheck: Int): Boolean =
       rules match
         case h :: t => (h.firstRule.contains(numCheck) || h.secondRule.contains(numCheck)) || boolComply(t, numCheck)
@@ -33,7 +36,6 @@ case class Ticket(numbers: List[Int]):
     this.numbers.filterNot(x => boolComply(rules, x))
 
   def complyWithRule(rules: List[TicketRules]): Boolean =
-
     def boolComply(rules: List[TicketRules], numCheck: Int): Boolean =
       rules match
         case h :: t => (h.firstRule.contains(numCheck) || h.secondRule.contains(numCheck)) || boolComply(t, numCheck)
@@ -41,8 +43,40 @@ case class Ticket(numbers: List[Int]):
 
     this.numbers.map(x => boolComply(rules, x)).contains(false)
 
+  def correctRulesForField(rules: List[TicketRules]): List[List[FieldName]] =
+    def recFilterFieldNames(rules: List[TicketRules], numCheck: Int): List[FieldName] =
+      rules match
+        case h :: t => if (h.firstRule.contains(numCheck) || h.secondRule.contains(numCheck))
+                        h.name :: recFilterFieldNames(t, numCheck) else "" :: recFilterFieldNames(t, numCheck)
+        case Nil    => Nil
 
-case class TicketRules(name: String, firstRule: Range, secondRule: Range)
+    this.numbers.map(x => recFilterFieldNames(rules, x))
+
+
+def checkPerTicketLocation(fieldNames: List[List[List[FieldName]]]) =
+  val numLocations = fieldNames.length
+  val names = fieldNames.flatten.flatten.distinct.filterNot(_ == "")
+  val numOccurrences = fieldNames.transpose.map(x => x.flatten.filterNot(_ == "").zipWithIndex.groupBy(_._1).transform((_, v) => v.length).toList).zipWithIndex
+
+  def findSolutions(input: List[(List[(FieldName, Int)], Int)], remainingNames: List[FieldName], accResult: List[(FieldName, Int)]): List[(FieldName, Int)] =
+
+    val result = findOneUniqueSolution(input)
+
+    val acc = result :: accResult
+
+    val newNames = remainingNames.filterNot(_ == result._1)
+
+    if (newNames.length == 0) acc else
+      findSolutions(input.filterNot(x => x._2 == result._2).map{case (x, y) => (x.filterNot(z => z._1 == result._1), y)}, newNames, acc)
+
+  def findOneUniqueSolution(input: List[(List[(FieldName, Int)], Int)]): (FieldName, Int)  =
+    input match
+      case h :: t => val check = h._1.filter(x => x._2 == numLocations) ; if (check.length == 1) (check.head._1, h._2) else
+        findOneUniqueSolution(t)
+      case Nil    => sys.error("found nothing")
+
+  findSolutions(numOccurrences, names, List.empty: List[(FieldName, Int)])
+
 
 object AOC16 extends App:
   val input = Source.fromFile("src/main/resources/input_aoc16.txt").mkString(sep =  "").split("\n\n").toList
@@ -51,8 +85,18 @@ object AOC16 extends App:
 
   val (rules, myTicket, nearbyTickets) = input match
     case List(a, b, c) => (toTicketRules(a), toTicket(b), toTicket(c))
+    case _             => sys.error("incorrect input")
+
+  println("Answer part 1 = " + nearbyTickets.map(x => x.doesNotComplyWithRule(rules)).flatten.sum)
 
   val validTickets = nearbyTickets.filterNot(x => x.complyWithRule(rules))
 
-  println("Answer part 1 = " + nearbyTickets.map(x => x.doesNotComplyWithRule(rules)).flatten.sum)
+  val intermediateSave = validTickets.map(x => x.correctRulesForField(rules))
+
+  val positions = checkPerTicketLocation(intermediateSave).filter{case (a, _) => a.startsWith("departure")}.map(x => x._2)
+
+  val fieldsOwnTicket = myTicket.head.numbers.zipWithIndex.filter{x => positions.contains(x._2)}
+
+  println("Answer part 2 = " + fieldsOwnTicket.map(x => x._1.toLong).foldRight(1: Long)(_ * _))
+
 
